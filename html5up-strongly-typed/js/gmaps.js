0360a564e92cbaebc0190d1f4c4efddccc4a77f8
UmbraDraconis <smlam@csupomona.edu>
2015-03-03 23:46:22 -0800

    Did a lot of things to a lot of pages
    Fixed google maps
    fixed locations
    updated wholesale
    updated menu
    changed spelling of wholesale
    made almost everything to be mobile and desktop compatible

--- html5up-strongly-typed/js/gmaps.js
+++ html5up-strongly-typed/js/gmaps.js
@@ -1,11 +1,21 @@
-function initialize() {
-				var mapCanvas = document.getElementById('map-canvas');
-				var mapOptions = {
-					center: new google.maps.LatLng(34.0866457,-117.8727476,449),
-					zoom: 15,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
-				}
-				var map = new google.maps.Map(mapCanvas, mapOptions)
-			}
-			google.maps.event.addDomListener(window, 'load', initialize);
+function initialize() 
+{
+	var mapCanvas = document.getElementById('map-canvas');
+	var myLatlng = new google.maps.LatLng(34.0866457,-117.8727476,449);
+
+	var mapOptions = 
+	{
+		zoom: 15,
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+	
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
